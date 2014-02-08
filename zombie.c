3f5d33ce9ad0bdd3cc0ef5a828c5f801d5831de1
dkdfirefly <dkdfirefly@gmail.com>
2014-02-08 15:27:44 -0500

    formatting changes

--- zombie.c
+++ zombie.c
@@ -4,14 +4,14 @@
 
 int main ()
 {
-    pid_t child_pid;
+  pid_t child_pid;
 
-      child_pid = fork ();
-        if (child_pid > 0) {
-              sleep (60);
-                }
-          else {
-                exit (0);
-                  }
-            return 0;
+  child_pid = fork ();
+  if (child_pid > 0) {
+    sleep (60);
+  }
+  else {
+    exit (0);
+  }
+  return 0;
 }
